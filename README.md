4216d2e3caf4541784d35c0212873a34f44942cd
AliciaAPerez <72763332+AliciaAPerez@users.noreply.github.com>
2021-02-19 01:48:16 -0600

    updating

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # UFO_Data_Website_Formatting_with_JavaScript
 UFO Data Website Formatting with JavaScript
+
+## Table of Contents 
+* [General Info](#General-info)  
+* [Technologies](#technologies)  
+* [Setup](#setup)  
+* [Data Sources](#data-sources)
+* [Overview](#overview)  
+
+## General Info
+This code is make a webpage for the data set revolving around UFO sightings. The code used JavaScript and HTML to make the web page. 
+
+## Technologies
+Project is created with:  
+* HTML5
+* Bootstrap 5.0.0
+* JavaScript 1.5
+
+## Setup
+To run the code live, start the html file in a gitbash window. The other files can viewed in Visual Code. This can also be ran using GitHub Pages found at this link: https://aliciaaperez.github.io/UFO_Data_Website_Formatting_with_JavaScript/
+
+## Data Sources 
+The data for the UFO sightings is in a javaScript labled data.js.
+
+## Overview
+Upon opening the html index page for this code, you will see all the data is supplied on the main page. There are filter options on the left which can be used to filter the data.
--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,11 @@
 // from data.js
 var tableData = data;
-// console.log(tableData)
 
+//selecting the section for the table
 let tbody = d3.select("tbody");
-//let table = d3.select("#ufo-table");
 let table = d3.select("#ufo-table")
 
+//insert data into HTML for view upon opening page
 tableData.forEach(({datetime, city, state, country, shape, durationMinutes, comments}) =>{
     let row = tbody.append("tr");
     row.append("td").text(datetime);
@@ -17,12 +17,14 @@ tableData.forEach(({datetime, city, state, country, shape, durationMinutes, comm
     row.append("td").text(comments);
 });
 
+//set buttons and forms to correct fields
 let button = d3.select("#filter-btn");
 let form1 = d3.select("#date-form");
 let form2 = d3.select("#city-form");
 let form3 = d3.select("#state-form");
 let form4 = d3.select("#country-form");
 let form5 = d3.select("#shape-form");
+//turn on events
 button.on("click", filterdata);
 form1.on("submit", filterdata);
 form2.on("submit", filterdata);
@@ -30,6 +32,7 @@ form3.on("submit", filterdata);
 form4.on("submit", filterdata);
 form5.on("submit", filterdata);
 
+//filterdata function
 function filterdata(){
     d3.event.preventDefault();
     let inputElement1 = d3.select("#datetime");
@@ -42,7 +45,7 @@ function filterdata(){
     let inputValue3 = inputElement3.property("value");
     let inputValue4 = inputElement4.property("value");
     let inputValue5 = inputElement5.property("value");
-    //blank forms to pull back all data
+    //input value length = 0 means it is blank, and if all are blank forms, then this pull back all data
     if (inputValue1.length + inputValue2.length + inputValue3.length + inputValue4.length + inputValue5.length === 0){
         tbody.html("");
         console.log("no info given");
@@ -58,15 +61,21 @@ function filterdata(){
             row.append("td").text(comments);
         
     })}
+    //filtering data for new table
     else {
+    // emptying table
     tbody.html("");
+    //console log input forms
     console.log(`${inputValue1}, ${inputValue2}, ${inputValue3}, ${inputValue4}, ${inputValue5}`);
+    //set filteredData variable - Will not work without this if no date is entered due to the way the code is set to filter
     let filteredData = tableData
     
+    //if the date form is blank, it will log it and move on to the next form
     if (inputValue1.length === 0) {console.log("no input for date");}
     else { 
     filteredData = tableData.filter(tableData => tableData.datetime === inputValue1);
     }
+    //repeats above for each form
     if (inputValue2.length === 0) {console.log("no input for city");}
     else {
     filteredData = filteredData.filter(filteredData => filteredData.city === inputValue2);
@@ -83,7 +92,9 @@ function filterdata(){
     else{
     filteredData = filteredData.filter(filteredData => filteredData.shape === inputValue5);
     }
+    //console log results
     console.log(filteredData);
+    //set up table for the filtered data
     filteredData.forEach(({datetime, city, state, country, shape, durationMinutes, comments}) =>{
         let row = tbody.append("tr");
         row.append("td").text(datetime);
